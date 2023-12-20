155f0f2f832f46f0579354ce4440ebded6ab648e
Mansur Azatbek <mansur62624@gmail.com>
2023-12-20 21:04:11 +0600

    docs: documentation updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! # **About**
+//!
+//! `get_chunk` is a library designed to create file iterators or streams (asynchronous iterators).
+//! It specializes in efficient file chunking, allowing users to retrieve chunks of data each time `get_chunk` is called.
+//!
+//! **Key Features:**
+//! - **File Chunking:** Seamlessly divide files, including large ones, into chunks with each "Next" iteration.
+//! - **Automatic chunking:** Dynamically adjusts chunk sizes for optimal performance.
+//! Efficiently manages memory by limiting large chunks to 85% of available free RAM.
+//! - **Modes:** Choose between automatic or manual tuning based on percentage or number of bytes.
+//!
+//! ---
+//! ***Important Notice:***
+//!
+//! Iterators created by `get_chunk` do not store the entire file in memory, especially for large datasets.
+//! Their purpose is to fetch data from files, even when dealing with substantial sizes, by reading in chunks.
+//!
+//! **Key Points:**
+//! - **Limited File Retention:** Creating an iterator for a small file might result in obtaining all data, depending on the OS.
+//!   However, this doesn't guarantee file persistence after iterator creation.
+//! - **Deletion Warning:** Deleting a file during iterator or stream iterations will result in an error.
+//!   These structures do not track the last successful position.
+//! - **No File Restoration:** Attempting to restore a deleted file during iterations is not supported.
+//!   These structures do not keep track of the file's original state.
 mod chunk;
 
 pub use chunk::data_chunk::ChunkSize;
