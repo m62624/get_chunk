0b5237bae258721969c7832d8575222963f9d0c6
Mansur Azatbek <mansur62624@gmail.com>
2023-12-20 20:28:09 +0600

    test: added tests

--- tests/iterator.rs
+++ tests/iterator.rs
@@ -11,18 +11,19 @@ mod size_format {
 
     #[test]
     pub fn iter_t_0() -> io::Result<()> {
-        let chunk_size = 300.0;
+        let chunk_size = 150.0;
         let file_orig = FileTest::create_file_with_size(
             FILE_TEST,
-            IECUnit::new(10.0, IECSize::Mebibyte).get_values().1,
+            IECUnit::new(960.0, IECSize::Kibibyte).get_values().1,
         )?;
 
-        let bytes_size: usize = IECUnit::new(chunk_size, IECSize::Kibibyte).into();
+        let file_iter = FileIter::new(file_orig.path.as_str())?.set_mode(ChunkSize::Bytes(
+            IECUnit::new(chunk_size, IECSize::Kibibyte).into(),
+        ));
 
-        let file_iter =
-            FileIter::new(file_orig.path.as_str())?.set_mode(ChunkSize::Bytes(bytes_size));
         let mut elements = file_iter.collect::<io::Result<Vec<_>>>()?;
         elements.pop();
+
         for chunk in elements {
             assert_eq!(
                 chunk.len(),
@@ -34,9 +35,31 @@ mod size_format {
 
     #[test]
     pub fn iter_t_1() -> io::Result<()> {
+        let chunk_size = 144.0;
         let file_orig = FileTest::create_file_with_size(
             FILE_TEST,
-            IECUnit::new(100.0, IECSize::Kibibyte).get_values().1,
+            IECUnit::new(960.0, IECSize::Kibibyte).get_values().1,
+        )?;
+
+        let file_iter = FileIter::new(file_orig.path.as_str())?.set_mode(ChunkSize::Percent(15.0));
+
+        let mut elements = file_iter.collect::<io::Result<Vec<_>>>()?;
+        elements.pop();
+
+        for chunk in elements {
+            assert_eq!(
+                chunk.len(),
+                IECUnit::new(chunk_size, IECSize::Kibibyte).get_values().1 as usize
+            );
+        }
+        Ok(())
+    }
+
+    #[test]
+    pub fn iter_t_2() -> io::Result<()> {
+        let file_orig = FileTest::create_file_with_size(
+            FILE_TEST,
+            IECUnit::new(700.0, IECSize::Kibibyte).get_values().1,
         )?;
 
         let mut file_from_chunks = FileTest::default();
